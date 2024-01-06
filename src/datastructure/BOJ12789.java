c34234bd484fff2b36437f9fa645e3ec8cf7b495
anjeongmin <102chimee@gmail.com>
2024-01-06 14:52:37 +0900

    7일차

--- src/datastructure/BOJ12789.java
+++ src/datastructure/BOJ12789.java
@@ -1,65 +1,56 @@
 package datastructure;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BOJ12789 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
-        }
+
+        Queue<Integer> queue = new LinkedList<>();
         Stack<Integer> stack = new Stack<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            queue.add(Integer.parseInt(st.nextToken()));
+        }
+
         int index = 1;
-        int i = 0;
-        boolean check = false;
-        while(index < N){
 
-            while(!check){
-                if(A[i]==index){
-                    index++;
-                    check = true;
-                }
-                else if(!stack.isEmpty()){
-                    if(stack.peek()==index){
-                        index++;
-                        stack.pop();
-                        check = true;
-                    }
-                    else{
-                        if(i<=N-1){
-                            stack.push(A[i]);
-                        }
-                        else{
-                            System.out.println("Sad");
-                            break;
-                        }
-
-                    }
-                }
-                else {
-                    stack.push(A[i]);
-                }
-                if(i<N-1){
-                    i++;
-                }
+        while(!queue.isEmpty())
+        {
+            if(queue.peek() == index)
+            {
+                queue.poll();
+                index++;
             }
-            if(i==N-1 && stack.isEmpty()){
+            else if(!stack.isEmpty() && stack.peek() == index)
+            {
+                stack.pop();
+                index++;
+            }
+            else{
+                stack.push(queue.poll());
+            }
+        }
+        while(!stack.isEmpty())
+        {
+            if(stack.peek() == index)
+            {
+                stack.pop();
+                index++;
+            }
+            else{
                 System.out.println("Sad");
-                break;
+                return;
             }
-            check = false;
         }
-        if(stack.isEmpty()){
-            System.out.println("Nice");
-        }
-
-
+        System.out.println("Nice");
     }
-}
+    }
